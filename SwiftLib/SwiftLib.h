cc69b01dbae8c2e1bc1dfd787cccd03dffe9b0eb
Vaibhav Meshram <vaibhav.meshram@asiapay.com>
2022-12-14 11:11:56 +0530

    added files
    added files to git hub

---
+++ SwiftLib/SwiftLib.h
@@ -0,0 +1,18 @@
+//
+//  SwiftLib.h
+//  SwiftLib
+//
+//  Created by APIN Mac on 13/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLib.
+FOUNDATION_EXPORT double SwiftLibVersionNumber;
+
+//! Project version string for SwiftLib.
+FOUNDATION_EXPORT const unsigned char SwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLib/PublicHeader.h>
+
+
